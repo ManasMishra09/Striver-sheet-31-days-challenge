807d523b8cb4f49bd954c5924e056eaead94cf0f
Manas Mishra <92935580+ManasMishra09@users.noreply.github.com>
2023-07-01 23:34:54 +0530

    p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,48 @@
+// The given code is an implementation of the "setZeroes" function, which modifies a matrix by setting the entire row and column to zero if any element in that row or column is zero.
+
+// Here's how the code works:
+
+// It initializes a variable "colm" to 1, which will be used to track if the first column of the matrix needs to be set to zero later.
+
+// It retrieves the number of rows and columns in the matrix using the "size" function on the matrix vector.
+
+// The code then iterates over each element in the matrix, starting from the top-left corner (index 0,0). For each element, it checks if it's zero:
+
+// If the element is zero and it's in the first column (j=0), it sets "colm" to 0. This is because we want to track if the first column needs to be set to zero based on the elements in the first column itself.
+
+// If the element is zero and it's not in the first column (j>0), it sets the corresponding elements in the first row and first column to zero. This is done by assigning 0 to "matrix[i][0]" and "matrix[0][j]".
+
+// After marking the rows and columns that need to be set to zero in the first row and first column, the code iterates over the matrix again, starting from the bottom-right corner (index row-1, col-1). For each element, it checks if either the corresponding element in the first row or the first column is zero:
+
+// If either "matrix[i][0]" or "matrix[0][j]" is zero, it means the current element should be set to zero. So it assigns 0 to "matrix[i][j]".
+// Finally, if "colm" is zero (indicating that the first column needs to be set to zero), the code iterates over the first column and sets all elements to zero.
+
+// The code effectively sets the required rows and columns to zero based on the presence of zeros in the matrix.
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int colm=1;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++ ){
+            if(matrix[i][0]==0)colm=0;
+
+            for(int j=1;j<col;j++ ){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=matrix[0][j]=0;
+                }
+        }
+        }
+
+        for(int i=row-1;i>=0;i--){
+
+            for(int j=col-1;j>=1;j--){
+                if( matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if(colm==0)matrix[i][0]=0;
+        }
+    }
+};
