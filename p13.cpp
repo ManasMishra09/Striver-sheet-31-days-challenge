120db6547f9354e4533dbf9cb2fbe5cc881a99fb
Manas Mishra <92935580+ManasMishra09@users.noreply.github.com>
2023-07-08 11:32:15 +0530

    Add files via upload

---
+++ p13.cpp
@@ -0,0 +1,25 @@
+//moore voting algorithm approach
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         int count = 0;
+        int el = 0;
+
+        for (int i=0;i<nums.size();i++) {
+            if (count == 0) {
+                count=1;
+                el=nums[i];
+            }
+
+          else if (nums[i] == el) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return el;
+    }
+};
