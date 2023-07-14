a429930e07d867f77015671ee5e354328e35f117
Manas Mishra <92935580+ManasMishra09@users.noreply.github.com>
2023-07-14 23:20:36 +0530

    Add files via upload

---
+++ p28(return Node contains cycle in LL).cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* extra = head;
+        while(fast->next!=NULL && fast->next->next !=NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                while(slow != extra){
+                    slow = slow->next;
+                    extra = extra->next;
+                }
+                return extra;
+            }
+        }
+        return NULL;
+    }
+};
