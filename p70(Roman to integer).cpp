08ad9dc6b379206c2aef2d11c495e3a7db64e842
Manas Mishra <92935580+ManasMishra09@users.noreply.github.com>
2023-07-29 23:37:19 +0530

    Add files via upload

---
+++ p70(Roman to integer).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        if (s.empty()) return 0;
+        
+        unordered_map<char, int> table = { 
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int result = table[s.back()];
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (table[s[i]] < table[s[i + 1]]) {
+                result -= table[s[i]];
+            }
+            else {
+                result += table[s[i]];
+            }
+        }
+        
+        return result;
+    }
+};
